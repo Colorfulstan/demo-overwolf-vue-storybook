54a9efb40b62bdc5d454cfe35dc0e45fc6bb3dd3
Jonas Krispin <jonas@krispin.it>
2018-11-13 13:47:13 +0100

    adding stories for overwolf components

--- src/stories/index.stories.ts
+++ src/stories/index.stories.ts
@@ -1,24 +1,36 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { storiesOf } from '@storybook/vue'
-import { action } from '@storybook/addon-actions'
-
-import AdminWindow from '../Admin.window.vue'
-import SubWindow from '../Sub.window.vue'
-import MainWindow from '../Main.window.vue'
+import { storiesOf } from '@storybook/vue';
+import { action }    from '@storybook/addon-actions';
+import AdminWindow   from '../Admin.window.vue';
+import SubWindow     from '../Sub.window.vue';
+import MainWindow    from '../Main.window.vue';
+import OwHeader      from '../shared/overwolf/ow.header.vue';
 
 storiesOf('Windows', module)
   .add('MainWindow', () => ({
     components: { MainWindow },
-    template: '<main-window></main-window>',
-    methods: { action: action('clicked') }
+    template  : '<main-window></main-window>',
+    methods   : { action: action('clicked') }
   }))
   .add('SubWindow', () => ({
     components: { SubWindow },
-    template: '<sub-window></sub-window>',
-    methods: { action: action('clicked') }
+    template  : '<sub-window></sub-window>',
+    methods   : { action: action('clicked') }
   }))
   .add('AdminWindow', () => ({
     components: { AdminWindow },
-    template: '<admin-window></admin-window>',
-    methods: { action: action('clicked') }
-  }))
+    template  : '<admin-window></admin-window>',
+    methods   : { action: action('clicked') }
+  }));
+
+storiesOf('Shared Components', module)
+  .add('Overwolf Header', () => ({
+    components: { OwHeader },
+    template  : '<ow-header></ow-header>',
+    methods   : {
+      closeWindow   : action('closeWindow clicked'),
+      minimize      : action('minimize clicked'),
+      showSupport   : action('showSupport'),
+      toggleMaximize: action('toggleMaximize')
+    }
+  }));
